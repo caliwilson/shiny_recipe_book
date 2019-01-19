0ad529a9cced8fea58f1a03abde4cb2765dbeb87
Mark Roepke <mroepke5@gmail.com>
2019-01-19 09:19:37 -0600

    Add download and clean functionality

--- app.R
+++ app.R
@@ -2,6 +2,10 @@ library(shiny)
 library(magrittr)
 
 recipe_df <- readr::read_delim("data/recipe_data.psv", delim = "|")
+sources <- unique(recipe_df$Source)
+cuisines <- unique(recipe_df$Cuisine)
+categories <- unique(recipe_df$Category)
+courses <- unique(recipe_df$Course)
 
 # Define UI for application that draws a histogram
 ui <- navbarPage(
@@ -15,10 +19,27 @@ ui <- navbarPage(
     "This web app is to serve as a modern recipe book - a way to save digital recipes over time. In addition, it's a way to record general information about those recipes to help inspire meals.",
     h2("Development information"),
     "This web app was built using R, Shiny, and DT - special thanks to Winston Chang for Yihue Xie for their work in developing these packages.",
-    "The code for the web app can be found here."
+    "The code for the web app can be found here.",
+    h2("Download data"),
+    "Download the recipe data as a pipe-separated text file.",
+    downloadButton("download", "Download Recipe Book")
   ),
   tabPanel(
-    "View Recipes"
+    "View Recipes",
+    sidebarLayout(
+      sidebarPanel(
+        h3("Filter Recipes"),
+        selectizeInput("fr_source", "Source", sort(sources), selected = sort(sources), multiple = T, options = NULL),
+        selectizeInput("fr_course", "Course", sort(courses), selected = sort(courses), multiple = T, options = NULL),
+        selectizeInput("fr_category", "Category", sort(categories), selected = sort(categories), multiple = T, options = NULL),
+        selectizeInput("fr_cuisine", "Cuisine", sort(cuisines), selected = sort(cuisines), multiple = T, options = NULL),
+        sliderInput("fr_rating_range", "Rating Range", min = 0, max = 10, value = c(0, 10), ticks = F),
+        sliderInput("fr_difficulty_range", "Difficulty Range", min = 0, max = 10, value = c(0, 10), ticks = F)
+      ),
+      mainPanel(
+        DT::dataTableOutput("filter_recipes_df")
+      )
+    )
   ),
   tabPanel(
     "Add a Recipe",
@@ -27,29 +48,37 @@ ui <- navbarPage(
         h3("Enter Recipe Information"),
         textInput("nr_title", "Title", value = ""),
         textInput("nr_website", "Website", value = ""),
-        textInput("nr_source", "Source", value = ""),
-        selectInput(
+        selectizeInput(
+          "nr_source", 
+          "Source", 
+          choices = sort(sources),
+          selected = NULL,
+          multiple = T,
+          options = list(create = T, maxItems = 1)
+        ),
+        selectizeInput(
             "nr_course", 
             "Course", 
-            choices = c("Appetizer", "Breakfast", "Dessert", "Entree", "Side", "Snack", "Other"),
-            selected = "Other"
+            choices = sort(courses),
+            selected = "Other",
+            multiple = T,
+            options = list(create = T, maxItems = 1)
         ),
-        selectInput(
+        selectizeInput(
             "nr_category", 
             "Category", 
-            choices = c(
-              "Bowl", "Burger", "Salad", "Pasta", "Pizza", "Sandwich", "Quesadilla", 
-              "Taco", "Soup", "Other", "Rice", "Risotto"),
-            selected = "Other"
+            choices = sort(categories),
+            selected = "Other",
+            multiple = T,
+            options = list(create = T, maxItems = 1)
         ),
-        selectInput(
+        selectizeInput(
             "nr_cuisine", 
-            "Type", 
-            choices = c(
-              "American", "Asian", "British/Irish", "Cajun", "Italian", 
-              "Mediterranean", "Mexican", "Southern", "Other"
-            ),
-            selected = "Other"
+            "Cuisine", 
+            choices = sort(cuisines),
+            selected = "Other",
+            multiple = T,
+            options = list(create = T, maxItems = 1)
         ),
         sliderInput("nr_rating", "Rating", min = 0, max = 10, value = 5, round = T, ticks = F),
         sliderInput("nr_difficulty", "Difficulty", min = 0, max = 10, value = 5, round = T, ticks = F),
@@ -68,7 +97,7 @@ ui <- navbarPage(
         selectizeInput(
           "sr_title", 
           "Title", 
-          choices = recipe_df$Title, 
+          choices = sort(recipe_df$Title), 
           selected = NULL, multiple = FALSE
         )      
       ),
@@ -79,8 +108,49 @@ ui <- navbarPage(
   )
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output, session) {
+  output$download <- downloadHandler(
+    filename = "recipe_book_data.psv",
+    content = function(file) {
+      readr::write_delim(
+        readr::read_delim("data/recipe_data.psv", delim = "|"),
+        file,
+        delim = "|"
+      )
+    }
+  )
+  observeEvent(
+    c(
+      input$fr_source,
+      input$fr_course,
+      input$fr_category,
+      input$fr_cuisine,
+      input$fr_difficulty_range,
+      input$fr_rating_range
+    ),
+    {
+      output$filter_recipes_df <- DT::renderDataTable(
+        {
+          df <- readr::read_delim("data/recipe_data.psv", delim = "|")
+          df %>% 
+            dplyr::select(Recipe = Website, Source, Course, Category, Cuisine, Rating, Difficulty) %>% 
+            dplyr::filter(
+              Source %in% input$fr_source,
+              Course %in% input$fr_course,
+              Category %in% input$fr_category,
+              Cuisine %in% input$fr_cuisine,
+              dplyr::between(Difficulty, input$fr_difficulty_range[1], input$fr_difficulty_range[2]),
+              dplyr::between(Rating, input$fr_rating_range[1], input$fr_rating_range[2])
+            ) %>% 
+            DT::datatable(rownames = F, escape = F, style = "bootstrap")  %>% 
+            DT::formatStyle(
+              'Rating',
+              color = DT::styleInterval(c(3, 7), c('red', 'white', 'lightgreen'))
+            )
+        }, server = F
+      )
+    }
+  )
   observeEvent(
     input$sr_title,
     {
@@ -119,7 +189,7 @@ server <- function(input, output, session) {
               htmltools::withTags(
                 div(
                   HTML(
-                    'Similarity scores provided by Gower\'s Similarity Coefficient <a href=\"http://members.cbio.mines-paristech.fr/~jvert/svn/bibli/local/Gower1971general.pdf\" target=\"_blank\" class=\"class2\">Gower (1971)</a>'
+                    'Similarity scores provided by Gower\'s Similarity Coefficient <a href=\"http://members.cbio.mines-paristech.fr/~jvert/svn/bibli/local/Gower1971general.pdf\" target=\"_blank\" class=\"class2\">(Gower 1971)</a>'
                   )
                 )
               )
@@ -135,6 +205,7 @@ server <- function(input, output, session) {
       df <- readr::read_delim("data/recipe_data.psv", delim = "|")
       df %>% 
         dplyr::select(-Title) %>% 
+        dplyr::rename(Recipe = Website) %>% 
         DT::datatable(rownames = FALSE, escape = FALSE, style = "bootstrap") %>% 
         DT::formatStyle(
           'Rating',
@@ -142,10 +213,11 @@ server <- function(input, output, session) {
         )
     }, server = FALSE
   )
-  recipe_df_proxy <- DT::dataTableProxy('recipes_df')
+  recipes_df_proxy <- DT::dataTableProxy('recipes_df')
   observeEvent(
     input$nr_submit, 
     {
+      # Update data
       new_row <- tibble::tibble(
           Title = input$nr_title,
           Website = paste0('<a href="', input$nr_website, '" target="_blank" class="class2">', input$nr_title,'</a>'),
@@ -156,20 +228,35 @@ server <- function(input, output, session) {
           Rating = input$nr_rating,
           Difficulty = input$nr_difficulty
       )
-      new_row %>% 
+      new_df <- new_row %>% 
         dplyr::bind_rows(
           readr::read_delim("data/recipe_data.psv", delim = "|")
-        ) %>% 
-        readr::write_delim("data/recipe_data.psv", delim = "|")
-      recipes_df_proxy %>% DT::addRow(new_row)
+        ) 
+      readr::write_delim(new_df, "data/recipe_data.psv", delim = "|")
+      
+      # Update UI
+      new_titles <- sort(new_df$Title)
+      new_sources <- sort(unique(new_df$Source))
+      new_cuisines <- sort(unique(new_df$Cuisine))
+      new_categories <- sort(unique(new_df$Category))
+      new_courses <- sort(unique(new_df$Course))
+      
       updateTextInput(session, "nr_title", value = "")
       updateTextInput(session, "nr_website", value = "")
-      updateTextInput(session, "nr_source", value = "")
-      updateSelectInput(session, "nr_course", selected = "Other")
-      updateSelectInput(session, "nr_category", selected = "Other")
-      updateSelectInput(session, "nr_cuisine", selected = "Other")
+      updateSelectizeInput(session, "nr_source", selected = NULL, options = list(create = TRUE))
+      updateSelectizeInput(session, "nr_course", choices = new_courses, selected = "Other", options = list(create = TRUE))
+      updateSelectizeInput(session, "nr_category", choices = new_categories, selected = "Other", options = list(create = TRUE))
+      updateSelectizeInput(session, "nr_cuisine", choices = new_cuisines, selected = "Other", options = list(create = TRUE))
       updateSliderInput(session, "nr_rating", value = 5)
       updateSliderInput(session, "nr_difficulty", value = 5)
+      updateSelectizeInput(session, "fr_source", choices = new_sources, selected = new_sources)
+      updateSelectizeInput(session, "fr_course", choices = new_courses, selected = new_courses)
+      updateSelectizeInput(session, "fr_category", choices = new_categories, selected = new_categories)
+      updateSelectizeInput(session, "fr_cuisine", choices = new_cuisines, selected = new_cuisines)
+      updateSelectizeInput(session, "sr_title", choices = new_titles)
+      
+      # Update table
+      recipes_df_proxy %>% DT::addRow(new_row)
     }
   )
 }
